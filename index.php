8c435e46ee890ef1fbcccab2c09bacb5ed397963
fentdevsex <fentdevelopment@gmail.com>
2023-05-27 22:20:49 +0300

    connect styles

--- index.php
+++ index.php
@@ -11,9 +11,10 @@ echo 'start work';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <title>iSamp RolePlay</title>
 </head>
 <body>
-
+    <p>Information of Website</p>
 </body>
 </html>
